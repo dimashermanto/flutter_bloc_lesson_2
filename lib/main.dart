47c9da56614d6b82040f562fbca0b2533f2877f7
dimashermanto <hermanto.dph@gmail.com>
2021-04-07 11:12:15 +0700

    BlocConsumer

--- lib/main.dart
+++ lib/main.dart
@@ -41,66 +41,63 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: BlocListener<CounterCubit, CounterState>(
-        //Executed everytume a change occured in CounterState
-        listener: (BuildContext context, state) {
-          // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("State changed"), duration: Duration(milliseconds: 700),));
-          if(state.wasIncremented){
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Nambah bossku ?"), duration: Duration(milliseconds: 700),));
-          }else{
-            ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Okeeee"), duration: Duration(milliseconds: 700),));
-          }
-        },
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'Button Pushed',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                BlocBuilder<CounterCubit, CounterState>(
-                  builder: (BuildContext context, state) {
-                    if (state.counterValue < 0) {
-                      return Text(
-                        "Currently under zero (${state.counterValue.toString()})",
-                      );
-                    } else {
-                      return Text(
-                        state.counterValue.toString(),
-                      );
-                    }
-                  },
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    FloatingActionButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).decrement();
-                      },
-                      tooltip: 'Decrement',
-                      child: Icon(Icons.remove),
-                    ),
-                    FloatingActionButton(
-                      onPressed: () {
-                        BlocProvider.of<CounterCubit>(context).increment();
-                      },
-                      tooltip: 'Increment',
-                      child: Icon(Icons.add),
-                    )
-                  ],
-                )
-              ],
-            ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Button Pushed',
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              BlocConsumer<CounterCubit, CounterState>(
+                listener: (BuildContext context, state) {
+                  // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("State changed"), duration: Duration(milliseconds: 700),));
+                  if(state.wasIncremented){
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Nambah bossku ?"), duration: Duration(milliseconds: 700),));
+                  }else{
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Okeeee"), duration: Duration(milliseconds: 700),));
+                  }
+                },
+                builder: (BuildContext context, state) {
+                  if (state.counterValue < 0) {
+                    return Text(
+                      "Currently under zero (${state.counterValue.toString()})",
+                    );
+                  } else {
+                    return Text(
+                      state.counterValue.toString(),
+                    );
+                  }
+                },
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).decrement();
+                    },
+                    tooltip: 'Decrement',
+                    child: Icon(Icons.remove),
+                  ),
+                  FloatingActionButton(
+                    onPressed: () {
+                      BlocProvider.of<CounterCubit>(context).increment();
+                    },
+                    tooltip: 'Increment',
+                    child: Icon(Icons.add),
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
